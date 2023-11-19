d934a64be01f99abdd49ed503c7166381fa32957
Hizhniy <hizhniy@gmail.com>
2023-11-19 18:18:33 +0300

    Добавьте файлы проекта.

---
+++ LifeSpot/Startup.cs
@@ -0,0 +1,42 @@
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace LifeSpot
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Маппинг статических файлов
+
+                endpoints.MapCss();
+                endpoints.MapJs();
+                endpoints.MapHtml();
+                //endpoints.MapPics();
+
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "Static", "images")),
+                    RequestPath = "/Static/images"
+                });
+            });
+        }
+    }
+}
